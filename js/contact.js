20dad636a94021bc39342c68fccb19f40eed78ad
divergent_cix <tolu58@gmail.com>
2019-09-05 15:33:35 +0100

    Update contact.js

--- js/contact.js
+++ js/contact.js
@@ -1,3 +1,7 @@
+	$(document).ready(function(){
+		$('#contactForm').hide();
+	})
+
 	const contactForm = document.querySelector('#contact-form');
 	const contactBtn = document.querySelector('#contact-btn');
 	const title = document.querySelector('#title');
@@ -5,6 +9,7 @@
 	const email = document.querySelector('#email');
 	const msg = document.querySelector('#msg');
 	const errmsg = document.querySelector('.error');
+	const closepg = document.querySelector('.close');
 
 
 	 contactForm.addEventListener('submit', e => {
@@ -15,4 +20,8 @@
 			
 			e.textContent('Please check that your Name is atleast 4 characters<br> and your message is atleast 20 characters');
 		}
+	 });
+
+       	 closepg.addEventListener('click', e => {
+	 	style.display = none;
 	 });
