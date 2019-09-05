9dbc5945947882428272d18b646fdf64b76b5b6a
tolu-six <tolu58@gmail.com>
2019-09-05 14:55:47 +0100

    CV updated

---
+++ js/contact.js
@@ -0,0 +1,18 @@
+	const contactForm = document.querySelector('#contact-form');
+	const contactBtn = document.querySelector('#contact-btn');
+	const title = document.querySelector('#title');
+	const contactName = document.querySelector('#name');
+	const email = document.querySelector('#email');
+	const msg = document.querySelector('#msg');
+	const errmsg = document.querySelector('.error');
+
+
+	 contactForm.addEventListener('submit', e => {
+		
+	 	e.preventDefault();
+
+		if(contactName.value.length < 4 || msg.value.length < 20){
+			
+			e.textContent('Please check that your Name is atleast 4 characters<br> and your message is atleast 20 characters');
+		}
+	 });
